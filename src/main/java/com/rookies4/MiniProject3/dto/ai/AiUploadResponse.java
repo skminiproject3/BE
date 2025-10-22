2bdca28155f4599cd1fc23c12d611478fa83584f
NamEon <143885031+skadjs@users.noreply.github.com>
2025-10-22 14:15:17 +0900

    feat: ai 응답 관련 클래스 추가

---
+++ src/main/java/com/rookies4/MiniProject3/dto/ai/AiUploadResponse.java
@@ -0,0 +1,17 @@
+package com.rookies4.MiniProject3.dto.ai;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AiUploadResponse {
+    private String message;
+
+    @JsonProperty("pdf_paths")
+    private List<String> pdfPaths;
+}
